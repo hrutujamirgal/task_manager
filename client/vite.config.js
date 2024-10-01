38e9c9a1ac57705e3627db1206cadde122c150ea
hrutujamirgal <hrutuja.mirgal21@vit.edu>
2024-10-01 20:29:07 +0530

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
